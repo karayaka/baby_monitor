5f75a0216c8868ba93630137552d7262a43cdf43
cagrikarayaka <cagri.karayaka@gmail.com>
2025-05-29 18:14:41 +0300

    free trn vpn

--- lib/core/app_tools/project_const.dart
+++ lib/core/app_tools/project_const.dart
@@ -40,9 +40,29 @@ class WebrtcConnectionConst {
       {'urls': 'stun:stun2.l.google.com:5349'},
       {'urls': 'stun:stun3.l.google.com:3478'},
       {
-        "urls": "turn:openrelay.metered.ca:80",
-        "username": "openrelayproject",
-        "credential": "openrelayproject",
+        "url": 'turn:numb.viagenie.ca',
+        "credential": 'muazkh',
+        "username": 'webrtc@live.com',
+      },
+      {
+        "url": 'turn:192.158.29.39:3478?transport=udp',
+        "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        "username": '28224511:1379330808',
+      },
+      {
+        "url": 'turn:192.158.29.39:3478?transport=tcp',
+        "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        "username": '28224511:1379330808',
+      },
+      {
+        "url": 'turn:turn.bistri.com:80',
+        "credential": 'homeo',
+        "username": 'homeo',
+      },
+      {
+        "url": 'turn:turn.anyfirewall.com:443?transport=tcp',
+        "credential": 'webrtc',
+        "username": 'webrtc',
       },
     ],
   };
