182e78bc41fa49f5fa279974437c91d81c179d28
cagrikarayaka <cagri.karayaka@gmail.com>
2025-05-30 17:36:15 +0300

    free trun test 5

--- lib/core/app_tools/project_const.dart
+++ lib/core/app_tools/project_const.dart
@@ -30,39 +30,27 @@ class HubMethods {
 
 class WebrtcConnectionConst {
   static const config = {
-    'iceServers': [
-      {'urls': 'stun:stun.l.google.com:19302'},
-      {'urls': 'stun:stun.l.google.com:19302'},
-      {'urls': 'stun:stun.l.google.com:5349'},
-      {'urls': 'stun:stun1.l.google.com:3478'},
-      {'urls': 'stun:stun1.l.google.com:5349'},
-      {'urls': 'stun:stun2.l.google.com:19302'},
-      {'urls': 'stun:stun2.l.google.com:5349'},
-      {'urls': 'stun:stun3.l.google.com:3478'},
+    "iceServers": [
+      {"urls": "stun:stun.relay.metered.ca:80"},
       {
-        "url": 'turn:numb.viagenie.ca',
-        "credential": 'muazkh',
-        "username": 'webrtc@live.com',
+        "urls": "turn:global.relay.metered.ca:80",
+        "username": "5bd7dda427cc7c367a6303f3",
+        "credential": "noUoSsqd//7KHSTD",
       },
       {
-        "url": 'turn:192.158.29.39:3478?transport=udp',
-        "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        "username": '28224511:1379330808',
+        "urls": "turn:global.relay.metered.ca:80?transport=tcp",
+        "username": "5bd7dda427cc7c367a6303f3",
+        "credential": "noUoSsqd//7KHSTD",
       },
       {
-        "url": 'turn:192.158.29.39:3478?transport=tcp',
-        "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        "username": '28224511:1379330808',
+        "urls": "turn:global.relay.metered.ca:443",
+        "username": "5bd7dda427cc7c367a6303f3",
+        "credential": "noUoSsqd//7KHSTD",
       },
       {
-        "url": 'turn:turn.bistri.com:80',
-        "credential": 'homeo',
-        "username": 'homeo',
-      },
-      {
-        "url": 'turn:turn.anyfirewall.com:443?transport=tcp',
-        "credential": 'webrtc',
-        "username": 'webrtc',
+        "urls": "turns:global.relay.metered.ca:443?transport=tcp",
+        "username": "5bd7dda427cc7c367a6303f3",
+        "credential": "noUoSsqd//7KHSTD",
       },
     ],
   };
