34784a0b2fb0df14ca81118cb108c7da77b4ca69
cagrikarayaka <cagri.karayaka@gmail.com>
2025-06-24 20:26:05 +0300

    safe areea

--- lib/views/pages/viewer_pages/viewer_page.dart
+++ lib/views/pages/viewer_pages/viewer_page.dart
@@ -8,33 +8,35 @@ class ViewerPage extends GetView<ViewerController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Obx(() {
-        if (controller.isConnect.value == 0) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (controller.isConnect.value == 2) {
-          return Center(child: Text("mb008".tr));
-        }
-        return Stack(
-          children: [
-            // Sol üstte kapatma butonu
-            Positioned(
-              top: 10,
-              left: 10,
-              child: IconButton(
-                onPressed: () => Get.back(),
-                icon: const Icon(Icons.close, color: Colors.black),
+    return SafeArea(
+      child: Scaffold(
+        body: Obx(() {
+          if (controller.isConnect.value == 0) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (controller.isConnect.value == 2) {
+            return Center(child: Text("mb008".tr));
+          }
+          return Stack(
+            children: [
+              // Sol üstte kapatma butonu
+              Positioned(
+                top: 10,
+                left: 10,
+                child: IconButton(
+                  onPressed: () => Get.back(),
+                  icon: const Icon(Icons.close, color: Colors.black),
+                ),
               ),
-            ),
-            Expanded(
-              child:
-                  controller.isConnect.value == 1
-                      ? RTCVideoView(controller.remoteRenderer)
-                      : Center(child: const CircularProgressIndicator()),
-            ),
-          ],
-        );
-      }),
+              Expanded(
+                child:
+                    controller.isConnect.value == 1
+                        ? RTCVideoView(controller.remoteRenderer)
+                        : Center(child: const CircularProgressIndicator()),
+              ),
+            ],
+          );
+        }),
+      ),
     );
   }
 }
